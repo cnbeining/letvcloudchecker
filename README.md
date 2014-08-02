ca32aa03883535dda564765ffdf231908f3307ef
cnbeining <http://www.cnbeining.com/>
2014-08-02 19:56:59 -0400

    V 0.1

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
 letvcloudchecker
 ================
-
 Yet another batch checker of video status on Letvcloud.
+
+Able to save the result in csv file.
+
+Usage
+------
+python3 letvcheck.py (-h)( -s) vu1 vu2 ...
+
+-s: Save to CSV file. -s save.csv
+
+-h: Help.
+
+Requirement
+-------
+- Python 3.3
+
+Author
+-----
+Beining, @cnbeining. http://www.cnbeining.com/
+
+License
+-----
+GPL v2 license.
+
+You are not supposed to public this script at any website whose location or whose server's location is Chinese Mainland.
+
+History
+----
+0.1: Start
+
+
+
+letvcloudchecker
+================
+批量查询乐视云视频情况。
+
+可以导出CSV文件。
+
+使用
+------
+python3 letvcheck.py (-h)( -s) vu1 vu2 ...
+
+-s: 保存到CSV. -s save.csv
+
+-h: 帮助.
+
+需求
+-------
+- Python 3.3
+
+作者
+-----
+Beining, @cnbeining. http://www.cnbeining.com/
+
+协议
+-----
+GPL v2 协议. 借完不开源的我会找过去的。
+
+禁止转载到墙内各种电子公告服务或服务器/位置在大陆的网站（包括但不限于百毒贴吧、AC 丧尸岛、各种论坛、微博等）。发现腿打折。
+
+历史
+----
+0.1: 第一个版本
---
+++ letvcheck.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+#coding:utf-8
+# Author: Beining  --<ACICFG>
+# Purpose: Batch check whether the Letvcloud file's statues.
+# Created: 08/02/2014
+
+import urllib.request
+import sys
+import os
+import json
+import hashlib
+import getopt
+
+global line_to_write
+line_to_write = ''
+
+#----------------------------------------------------------------------
+def check_upload(source_id):
+    """"""
+    line_to_write_this =''
+    message = ''
+    user_id = ''
+    video_name = ''
+    video_duration = ''
+    add_time = ''
+    type_avalable = ''
+    user_unique = ''
+    video_type_set = ''
+    str2Hash = 'cfflashformatjsonran0.7214574650861323uu2d8c027396ver2.1vu' + source_id + 'bie^#@(%27eib58'
+    sign = hashlib.md5(str2Hash.encode('utf-8')).hexdigest()
+    request_info = urllib.request.Request('http://api.letvcloud.com/gpc.php?&sign='+sign+'&cf=flash&vu='+source_id+'&ver=2.1&ran=0.7214574650861323&qr=2&format=json&uu=2d8c027396')
+    try:
+        response = urllib.request.urlopen(request_info)
+        data = response.read()
+        message = json.loads(data.decode('utf-8'))['message']
+        user_id = json.loads(data.decode('utf-8'))['data']['video_info']['user_id']
+        video_name = json.loads(data.decode('utf-8'))['data']['video_info']['video_name']
+        video_duration = json.loads(data.decode('utf-8'))['data']['video_info']['video_duration']
+        add_time = json.loads(data.decode('utf-8'))['data']['video_info']['add_time']
+        user_unique = json.loads(data.decode('utf-8'))['data']['user_info']['user_unique']
+        video_type_set = json.loads(data.decode('utf-8'))['data']['user_info']['video_type_set']
+        for i in json.loads(data.decode('utf-8'))['data']['video_info']['media']:
+            type_avalable = type_avalable + json.loads(data.decode('utf-8'))['data']['video_info']['media'][i]['video_type']
+    except:
+        return 'Cannot check '+source_id+' !'
+    line_to_write_this = source_id + ',' + message + ',' + video_name + ',' + video_duration + ',' + add_time + ',' + user_id + ',' + type_avalable
+    print(line_to_write_this)
+    return line_to_write_this
+
+#----------------------------------------------------------------------
+def usage():
+    """"""
+    print('''Usage:
+    python3 letvcheck.py (-h)( -s) vu1 vu2
+    -s: Save to CSV file.
+    -h: Help.''')
+
+
+if __name__=='__main__':
+    argv_list = []
+    argv_list = sys.argv[1:]
+    file_csv = ''
+    line_to_write = ''
+    try:
+        opts, args = getopt.getopt(argv_list, "hs:", ["help",'save'])
+    except getopt.GetoptError:
+        usage()
+        exit()
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            exit()
+        elif o in ('-s', '--save'):
+            file_csv = a
+            print('Saving your result to csv file '+a)
+            try:
+                argv_list.remove('-s')
+            except:
+                break
+            argv_list.remove(a)
+    #print(argv_list)
+    if file_csv is not '':
+        for vu in argv_list:
+            line_to_write = line_to_write + str(check_upload(vu)) + '\n'
+            line_to_write = line_to_write
+            f = open(file_csv, "w")
+            f.writelines(line_to_write)
+            f.close()
+    else:
+        for vu in argv_list:
+            check_upload(vu)
